9c75ac0fe033126062a2671a976c28623d141c5a
Andrew Riehm <ariehm@vulncheck.com>
2024-02-06 15:40:18 -0700

    Added build info to output.

--- cmd/hw/main.go
+++ cmd/hw/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
 func main() {
 	fmt.Printf("Hello, world!\n")
+	fmt.Printf("build: %s\n", getVCSInfo())
+}
+
+func getVCSInfo() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return setting.Value
+			}
+		}
+	}
+
+	return "could not retrieve build info"
 }
